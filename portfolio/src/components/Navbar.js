a2428387a2af3a3a744a02f8df2b2bac841e256b
Cristina Castro <cristinacastro87@gmail.com>
2020-12-31 15:20:20 +0100

    create components, add navbar

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const navbar = () => {
+  return (
+    <nav className="navbar">
+      <div>
+        <div>
+          <p>Cristina Castro</p>
+        </div>
+        <ul>
+          <li style={{ listStyle: "none" }}>
+            <Link to="/projects" style={{ textDecoration: "none" }}>
+              Projects
+            </Link>
+          </li>
+          <li style={{ listStyle: "none" }}>
+            <Link to="/projects" style={{ textDecoration: "none" }}>
+              About
+            </Link>
+          </li>
+          <li style={{ listStyle: "none" }}>
+            <Link to="/projects" style={{ textDecoration: "none" }}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default navbar;
