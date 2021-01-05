02b6894ebd42166855959a02b6d7f15bc6b93796
Cristina Castro <cristinacastro87@gmail.com>
2021-01-05 23:18:43 +0100

    project list styles

--- portfolio/src/components/ProjectList.js
+++ portfolio/src/components/ProjectList.js
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 
 export default function ProjectList({projects}) {
   return (
-      <div className="projects mt100">
-      <h1 className="ml50 mb20">Projects.</h1>
-          <div className="project-card">
+      <div>
+      <h1 className="ml50 mb20 mt100">Projects.</h1>
+          <div>
               {projects.map((eachProject, index) => {
                   return (
-                      <div key={eachProject.id}>
-                        <img src = {eachProject.picture} alt="project pic" width="700"/>
-                        <div className="mr50 ml50">
-                        <h2>{eachProject.title}</h2>
-                        <h6>{eachProject.intro}</h6>
-                        <p><Link to={{pathname:`/projects/${eachProject.id}`, state: {projectsState: projects}}}>See project > </Link></p>
+                      <div className="project-card" key={eachProject.id}>
+                        <div className="project-card-text ml10 mt10">
+                        <h6>{eachProject.title} |  {eachProject.intro}</h6>
+                        </div>
+                        <div className="project-card-img">
+                        <Link to={{pathname:`/projects/${eachProject.id}`, state: {projectsState: projects}}}><img src = {eachProject.picture} alt="project pic" width="700"/></Link>
                         </div>
                       </div>
                   )
