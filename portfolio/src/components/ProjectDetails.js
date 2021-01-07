c8b844703978c7fd794e922432f8371d2b40261d
Cristina Castro <cristinacastro87@gmail.com>
2021-01-07 14:18:55 +0100

    working on project detail view

--- portfolio/src/components/ProjectDetails.js
+++ portfolio/src/components/ProjectDetails.js
@@ -1,25 +1,37 @@
 import React from "react";
 
 export default function ProjectDetails(props) {
-  let getProject = id => props.location.state.projectsState.find(obj => obj.id === id);
-  console.log(props.location.state.projectsState, "idddd")
+  let getProject = (id) =>
+    props.location.state.projectsState.find((obj) => obj.id === id);
   const { id } = props.match.params;
-  console.log(props.match.params, "dfkdsf")
   const theProject = getProject(id);
-  console.log(theProject, "projecte");
   return (
     <div>
+    <div className="mt80 mb40 mr50 ml50">
+      <a href="/projects">
+        <img src="../img/back.png" width="15" />
+      </a>
       <h2>{theProject.title}</h2>
-      <p>{theProject.description}</p>
-       {theProject.allpictures.map((eachPicture, index) => {
-        return(
-          <div key={eachPicture.id}>
-          <img src={eachPicture} alt="project pic" width="700"/>
-          </div>
-        )
-      })} 
-      <a href="/projects">Back to projects</a>
+      <h6>
+        {theProject.title} | {theProject.intro}
+      </h6>
+      <h5>About the project</h5>
+      <p className="mt10">{theProject.description}</p>
+     
+          <div>
+      {theProject.technologies!=="" && <h5>Technologies</h5>}
+      <p className="mt10">{theProject.technologies}</p>
+      </div>
+    
     </div>
-
+      {theProject.allpictures.map((eachPicture, index) => {
+        return (
+          <div className = "product-detail-img" key={eachPicture.id}>
+            <img src={eachPicture} alt="project pic" width="700" />
+          </div>
+        );
+      })}
+      <a href = "">Live demo</a>
+    </div>
   );
 }
