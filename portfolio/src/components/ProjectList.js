79064da4efb2021b1c7277eac94b1367ce8f5497
Cristina Castro <cristinacastro87@gmail.com>
2021-01-07 13:13:15 +0100

    fixing project page

--- portfolio/src/components/ProjectList.js
+++ portfolio/src/components/ProjectList.js
@@ -8,13 +8,14 @@ export default function ProjectList({projects}) {
           <div>
               {projects.map((eachProject, index) => {
                   return (
-                      <div className="project-card" key={eachProject.id}>
-                        <div className="project-card-text ml10 mt10">
+                      <div className="project-card ml50 mr50" key={eachProject.id}>
+                        <div className="project-card-img">
+                        <img src = {eachProject.picture} alt="project pic" width="700"/>
+                        </div>
+                        <div className="project-card-text mt10 ">
                         <h6>{eachProject.title} |  {eachProject.intro}</h6>
                         </div>
-                        <div className="project-card-img">
-                        <Link to={{pathname:`/projects/${eachProject.id}`, state: {projectsState: projects}}}><img src = {eachProject.picture} alt="project pic" width="700"/></Link>
-                        </div>
+                        <p className="mb20"><Link to={{pathname:`/projects/${eachProject.id}`, state: {projectsState: projects}}}>See project ></Link></p>
                       </div>
                   )
               })}
